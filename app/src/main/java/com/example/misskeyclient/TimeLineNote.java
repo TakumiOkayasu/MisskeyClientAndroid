12441f5fb401a9e1bad38d36185f85e11d478aa2
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-07-03 22:09:25 +0900

    一応タイムライン上のNoteのイメージをざっくりと

--- app/src/main/java/com/example/misskeyclient/TimeLineNote.java
+++ app/src/main/java/com/example/misskeyclient/TimeLineNote.java
@@ -1,5 +1,29 @@
 package com.example.misskeyclient;
 
-public class TimeLineNote
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * TimeLine上で表示されるNoteのView
+ **/
+public class TimeLineNote extends RecyclerView.ViewHolder
 {
+	private View view;
+	private TextView screenName;
+	private TextView userName;
+	private ImageView icon;
+	private TextView noteText;
+	public TimeLineNote( @NonNull View itemView )
+	{
+		super( itemView );
+		this.view = itemView;
+		this.screenName = itemView.findViewById( R.id.screenName );
+		this.userName = itemView.findViewById( R.id.userName );
+		this.noteText = itemView.findViewById( R.id.noteText );
+		this.icon = itemView.findViewById( R.id.icon );
+	}
 }
